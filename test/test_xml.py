7bd2a5600d4f2b51a5ddc714bba7635cbe1e95d7
lichao127 <lichao127@github.com>
2023-02-23 16:28:12 -0800

    fix dir

--- test/test_xml.py
+++ test/test_xml.py
@@ -1,16 +1,18 @@
 import pytest
 from xmlunittest import XmlTestCase
+import os
+BASE_DIR=os.path.dirname(os.path.realpath(__file__))
 
 
 class CustomTestCase(XmlTestCase):
 
     def test_good_xml(self):
-        fc = open('good.xsd')
+        fc = open(os.path.join(BASE_DIR, 'good.xsd'))
         data = fc.read().encode()
         root = self.assertXmlDocument(data)
 
     def test_bad_xml(self):
-        fc = open('bad.xsd')
+        fc = open(os.path.join(BASE_DIR, 'bad.xsd'))
         data = fc.read().encode()
         with pytest.raises(AssertionError):
             self.assertXmlDocument(data)

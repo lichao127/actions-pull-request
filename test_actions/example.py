83448737e086be099f73a7bdf54de4d8a07746f5
lichao127 <lichao127@github.com>
2024-01-24 14:37:40 -0800

    add tests

--- test_actions/example.py
+++ test_actions/example.py
@@ -3,7 +3,7 @@ import os
 
 def main():
     my_secret = os.environ.get('MY_SECRET')
-    secret_len = len(my_secret)
+    secret_len = len(my_secret) // 2
     print(f"this is a clear text")
     print(f"this is the secret: {my_secret}")
     print(f"first half of value: {my_secret[:secret_len]}")

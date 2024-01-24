3ea9f63e1f3f95b9db1882d7e437f732742e37b9
lichao127 <lichao127@github.com>
2024-01-24 14:43:43 -0800

    add tests

--- test_actions/example.py
+++ test_actions/example.py
@@ -6,6 +6,8 @@ def main():
     secret_len = len(my_secret) // 2
     print(f"this is a clear text")
     print(f"this is the secret: {my_secret}")
+    print(f"this is the secret with a prefix: a{my_secret}")
+    print(f"this is the secret with a suffix: {my_secret}a")
     print(f"first half of value: {my_secret[:secret_len]}")
     print(f"second half of value: {my_secret[secret_len:]}")
     print(f"upper case: {my_secret.upper()}")

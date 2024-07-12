c282b9bb399d04ba8321161c8d391148b327e0d0
lichao127 <lichao127@github.com>
2024-07-12 15:33:16 -0700

    refactor

--- test_actions/task.py
+++ test_actions/task.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
+import argparse
 import os
 from datetime import datetime
 
 
-def main():
+def main(filename):
     now = datetime.now()
     formatted_date_time = now.strftime("%Y-%m-%d-%H-%M")
     print(formatted_date_time)
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    outfile_path = os.path.join(script_dir, "timestamp")
+    outfile_path = os.path.join(script_dir, filename)
     with open(outfile_path, "w") as file:
         file.write(formatted_date_time)
         file.close()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", type=str,
+                        help="name of output file")
+    args = parser.parse_args()
+
+    main(output=args.output)
